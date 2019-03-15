4f8e6ed83d8d10230ac8994f72091e97836ba8e7
MarcusTelecom <marcus.ufsj@gmail.com>
2019-03-14 21:37:16 -0300

    *Criando Formulário - Adicionando Botão Login - Criando método validate

--- app/src/main/java/telecom/marcus/com/projetoappmrs/MainActivity.java
+++ app/src/main/java/telecom/marcus/com/projetoappmrs/MainActivity.java
@@ -1,13 +1,56 @@
 package telecom.marcus.com.projetoappmrs;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatEditText;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Recuperando os componentes do activity_main
+    private AppCompatEditText editTextEmail;
+    private AppCompatEditText editTextSenha;
+
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutSenha;
+
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextEmail = (AppCompatEditText) findViewById(R.id.id_et_email);
+        textInputLayoutEmail = (TextInputLayout) findViewById(R.id.id_txtlayout_email);
+
+        editTextSenha = (AppCompatEditText) findViewById(R.id.id_et_senha);
+        textInputLayoutSenha = (TextInputLayout) findViewById(R.id.id_txtlayout_senha);
+
+        btnLogin = (Button) findViewById(R.id.id_btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateForm();
+            }
+        });
+    }
+
+    private void validateForm(){
+        if(editTextEmail.getText().toString().isEmpty()){
+            textInputLayoutEmail.setErrorEnabled(true);
+            textInputLayoutEmail.setError("Preencha com seu email");
+        }else {
+            textInputLayoutEmail.setErrorEnabled(false);
+        }
+
+        if (editTextSenha.getText().toString().isEmpty()){
+            textInputLayoutSenha.setErrorEnabled(true);
+            textInputLayoutSenha.setError("Digite sua senha");
+        }else {
+            textInputLayoutSenha.setErrorEnabled(false);
+        }
     }
 }
